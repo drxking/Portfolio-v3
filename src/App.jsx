163c924f21e591c028c55dafca6c74538835bcae
Sudip Acharya <drxking456@gmail.com>
2026-09-04 22:13:33 +0545

    Mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
-import { MoveRight, Power } from 'lucide-react';
+import { MoveRight } from 'lucide-react';
 
 import ImageRevealTrail from "./components/ImageRevealTrail"
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import gsap from 'gsap';
+
+import { ScrollTrigger } from "gsap/ScrollTrigger"
+import Lenis from 'lenis';
+import { ShinyButton } from "../components/ui/shiny-button"
+
+gsap.registerPlugin(ScrollTrigger)
 
 let skills = [
 
@@ -72,15 +79,83 @@ let skills = [
 
 ]
 
+
+let projects = [
+  {
+    name: "Taara",
+    img: "taraa.png",
+    desc: "A frontend clone project showcasing seamless animations using GSAP, smooth scrolling with Lenis, and modern design principles with Tailwind CSS.",
+    skills: [
+      "tailwind", "javascript", "gsap", "git"
+    ],
+    link:"https://taara-clone.sudipacharya456.com.np/"
+  },
+  {
+    name: "Payment",
+    img: "payment.png",
+    desc: "A modern, gesture-inspired UI that brings swipe-to-pay interactions and seamless transitions to the web.",
+    skills: [
+      "react", "tailwind", "javascript", "gsap","mongo"
+    ],
+    link:"https://payment.sudipacharya456.com.np/"
+  },
+  {
+    name: "Dakar",
+    img: "dakar.png",
+    desc: "A bold and interactive website that blends futuristic design with smooth motion, delivering a visually striking and immersive exploration experience.",
+    skills: [
+      "tailwind", "javascript","mongo", "gsap", "react"
+    ],
+    link:"https://dakar.sudipacharya456.com.np/"
+  },
+  {
+    name: "Wander",
+    img: "wander.png",
+    desc: "A modern web experience combining smooth animations, immersive scrolling, and elegant interactivity",
+    skills: [
+      "tailwind", "javascript", "gsap"
+    ],
+    link:"https://wanders.sudipacharya456.com.np/"
+  }
+]
+
 function App() {
+
+
+  useEffect(() => {
+    const lenis = new Lenis()
+
+    lenis.on("scroll", ScrollTrigger.update)
+
+    gsap.ticker.add((time) => {
+      lenis.raf(time * 1000)
+    })
+
+    gsap.ticker.lagSmoothing(0)
+
+    return () => {
+      gsap.ticker.remove((time) => {
+        lenis.raf(time * 1000)
+      })
+
+      lenis.destroy()
+    }
+  }, [])
+
+
   const [width, setwidth] = useState(window.innerWidth)
+
 
   window.addEventListener("resize", (e) => {
     setwidth(window.innerWidth)
   })
+
+  const hello = useRef(null)
+
+
   return (
     <main className='overflow-x-hidden'>
-      <nav className="fixed flex w-full md:flex-row flex-col gap-6 z-50 md:justify-between items-center p-8 md:px-20  ">
+      <nav className="absolute top-0 flex w-full md:flex-row flex-col gap-6 z-50 md:justify-between items-center p-8 md:px-20  ">
         <div className='relative w-fit h-fit'>
           <a href="https://github.com/drxking" target='__blank'>
             <svg data-component="Octicon" aria-hidden="true" focusable="false" className="octicon octicon-mark-github scale-200" width="26" height="26" fill="currentColor" display="inline-block" overflow="visible" style={{ "verticalAlign": "text-bottom" }}><path d="M10.226 17.284c-2.965-.36-5.054-2.493-5.054-5.256 0-1.123.404-2.336 1.078-3.144-.292-.741-.247-2.314.09-2.965.898-.112 2.111.36 2.83 1.01.853-.269 1.752-.404 2.853-.404 1.1 0 1.999.135 2.807.382.696-.629 1.932-1.1 2.83-.988.315.606.36 2.179.067 2.942.72.854 1.101 2 1.101 3.167 0 2.763-2.089 4.852-5.098 5.234.763.494 1.28 1.572 1.28 2.807v2.336c0 .674.561 1.056 1.235.786 4.066-1.55 7.255-5.615 7.255-10.646C23.5 6.188 18.334 1 11.978 1 5.62 1 .5 6.188.5 12.545c0 4.986 3.167 9.12 7.435 10.669.606.225 1.19-.18 1.19-.786V20.63a2.9 2.9 0 0 1-1.078.224c-1.483 0-2.359-.808-2.987-2.313-.247-.607-.517-.966-1.034-1.033-.27-.023-.359-.135-.359-.27 0-.27.45-.471.898-.471.652 0 1.213.404 1.797 1.235.45.651.921.943 1.483.943.561 0 .92-.202 1.437-.719.382-.381.674-.718.944-.943"></path></svg>
@@ -91,14 +166,18 @@ function App() {
         <ul className="flex  md:justify-center justify-between w-full items-center  md:w-auto  md:gap-12 gap-2 md:text-sm text-xs">
           {
             [
-              { link: "/", name: "Home" },
-              { link: "/", name: "Works" },
-              { link: "/", name: "Skills" },
-              { link: "/", name: "Resume" }
+              { link: "#", name: "Home" },
+              { link: "#works", name: "Works" },
+              { link: "#skills", name: "Skills" }
             ].map((e) => (
-              <li key={e.name} className="uppercase ">{e.name}</li>
+              <li key={e.name} className="uppercase ">
+                <a href={e.link}>{e.name}</a>
+              </li>
             ))
           }
+          <li className="uppercase ">
+            <a  href="Sudip_Acharya_Resume.pdf" download>Resume</a>
+          </li>
         </ul>
         <a href="" className='group h-[60%] md:flex hidden'><p className="uppercase flex items-center text-sm  gap-1">Hire Me <MoveRight className='group-hover:translate-x-2 transition inline-block' /></p></a>
       </nav>
@@ -125,7 +204,7 @@ function App() {
         </div>
       </div>
 
-      <div className='h-screen relative overflow-hidden grain-bg w-screen grid gap-0.5  items-end grid-cols-5  lg:grid-cols-11 '>
+      <div id='skills' className='h-screen relative overflow-hidden grain-bg w-screen grid gap-0.5  items-end grid-cols-5  lg:grid-cols-11 '>
 
         <div className='absolute cursiv bottom-5 text-center w-full uppercase font-bold text-8xl custom-text text-black/10 '>
           My <br /> Skills
@@ -140,20 +219,125 @@ function App() {
             }
 
             return (
-              <div
-                key={e.name}
-                className="bg-red-900/40 h-full flex flex-col justify-end "
-              >
-                <img
-                  style={{
-                    animationDelay: `${e.power / 5}s`,
-                  }}
-                  src={`/skills/${e.img}`}
-                  className="w-full float"
-                />
+              <Bar key={e.name} e={e} />
+            );
+          })
+        }
 
-                <div
-                  className="
+
+      </div>
+      <div id='works' className='bg-[#AB0910] pt-20 md:px-10 px-4'>
+        <h1 className='md:text-[11rem] text-[8rem] md:mb-10 leading-none custom-text uppercase font-extrabold invert cursiv'>My Works</h1>
+        <div className='  md:pt-10 grid  grid-cols-1 lg:grid-cols-2 gap-x-5 md:gap-y-40 gap-y-5'>
+          {
+            projects.map((e, idx) => {
+              if (idx % 2 == 0) {
+                return (
+                  <>
+                    <div className='h-auto md:mt-0 mt-20'>
+                      <img src={`/projects/${e.img}`} className='h-auto w-full' />
+                    </div>
+                    <div className='text-black'>
+                      <h1 className='md:text-8xl text-5xl uppercase custom-text font-bold invert'>{e.name}</h1>
+                      <p className='xl:w-2/4 lg:w-3/4 pt-4 md:pl-10 uppercase text-xs invert'>{e.desc}</p>
+                      <p className='md:pl-10 pt-10 flex  gap-2'>
+                        {e.skills?.map((ei, idxs) => (
+                          <img key={idxs} src={`/skills/${ei}.png`} className='h-14' alt={ei} />
+                        ))}
+                      </p>
+                      <a href={e.link}>
+                        <ShinyButton className='md:mt-10 mt-2 md:ml-10'>Visit Site</ShinyButton>
+                      </a>
+
+                    </div>
+                  </>
+                )
+              }
+              else {
+               if(window.innerWidth>=1024){
+                 return (
+                  <>
+                    <div className='flex flex-col text-black items-end'>
+                      <h1 className='md:text-8xl text-5xl uppercase custom-text invert font-bold'>{e.name}</h1>
+                      <p className='xl:w-2/4 lg:w-3/4 pt-4 md:pr-10 text-right invert'>{e.desc}</p>
+                      <p className='md:pr-10 pt-10 flex justify-end gap-2'>
+                        {e.skills?.map((ei, idxs) => (
+                          <img key={idxs} src={`/skills/${ei}.png`} className='h-14' alt={ei} />
+                        ))}
+                      </p>
+                      <a href={e.link}>
+                        <ShinyButton className='md:mt-10 mt-2 md:mr-10'>Visit Site</ShinyButton>
+                      </a>
+                    </div>
+                    <div className='h-auto md:mt-0 mt-20'>
+                      <img src={`/projects/${e.img}`} className='h-auto w-full' />
+                    </div>
+                  </>
+                )
+               }else{
+                 return (
+                  <>
+                  <div className='h-auto md:mt-0 mt-20'>
+                      <img src={`/projects/${e.img}`} className='h-auto w-full' />
+                    </div>
+                    <div className='flex flex-col text-black items-end'>
+                      <h1 className='md:text-8xl text-5xl uppercase custom-text invert font-bold'>{e.name}</h1>
+                      <p className='xl:w-2/4 lg:w-3/4 pt-4 md:pr-10 text-right invert'>{e.desc}</p>
+                      <p className='md:pr-10 pt-10 flex justify-end gap-2'>
+                        {e.skills?.map((ei, idxs) => (
+                          <img key={idxs} src={`/skills/${ei}.png`} className='h-14' alt={ei} />
+                        ))}
+                      </p>
+                      <a href={e.link}>
+                        <ShinyButton className='md:mt-10 mt-2 md:mr-10'>Visit Site</ShinyButton>
+                      </a>
+                    </div>
+                    
+                  </>
+                )
+               }
+              }
+            })
+          }
+        </div>
+      </div>
+
+    </main>
+
+  )
+}
+
+export default App
+
+
+const Bar = ({ e }) => {
+  let hello = useRef(null)
+  // useGSAP(()=>{
+  //   gsap.from(hello.current,{
+  //     height:"0",
+  //     delay:e.power/20,
+  //     scrollTrigger:{
+  //       trigger:".grain-bg" ,
+  //       start: "top 20%",
+  //     }
+  //   })
+  // })
+  return (
+    <div
+      key={e.name}
+      className="bg-red-900/40 h-full flex flex-col justify-end "
+    >
+      <img
+        style={{
+          animationDelay: `${e.power / 5}s`,
+        }}
+        src={`/skills/${e.img}`}
+        className="w-full float"
+      />
+
+      <div
+        ref={hello}
+        className="
             bg-linear-to-b
             from-black/70
             to-transparent
@@ -162,12 +346,12 @@ function App() {
             rounded-t-full
             shadow-custom
           "
-                  style={{
-                    height: `${e.power * 10 + 15}%`,
-                  }}
-                >
-                  <p
-                    className="
+        style={{
+          height: `${e.power * 10 + 15}%`,
+        }}
+      >
+        <p
+          className="
               absolute
               top-[15%]
               left-1/2
@@ -179,25 +363,14 @@ function App() {
               whitespace-nowrap
               custom-text2
             "
-                    style={{
-                      writingMode: "vertical-rl",
-                      textOrientation: "mixed",
-                    }}
-                  >
-                    {e.name}
-                  </p>
-                </div>
-              </div>
-            );
-          })
-        }
-
-
+          style={{
+            writingMode: "vertical-rl",
+            textOrientation: "mixed",
+          }}
+        >
+          {e.name}
+        </p>
       </div>
-
-    </main>
-
+    </div>
   )
 }
-
-export default App
